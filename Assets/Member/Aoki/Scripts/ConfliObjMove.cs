cb7152de7b7d125a7825fd7c0705a623722077fd
青木悠人 <TTC22C1020001@edu.tech.ac.jp>
2024-10-18 18:32:21 +0900

    プレイヤー引っ張り動作

--- Assets/Member/Aoki/Scripts/ConfliObjMove.cs
+++ Assets/Member/Aoki/Scripts/ConfliObjMove.cs
@@ -11,6 +11,14 @@ public class ConfliObjMove : MonoBehaviour
     private Vector2 mouseEndPos;
     private Vector2 startDirection;
 
+    [SerializeField]
+    private float maxLine;
+    [SerializeField]
+    private LineRenderer lineRen;
+    [SerializeField]
+    private LayerMask wall;
+
+
     public bool goFlag;
     // Start is called before the first frame update
     void Start()
