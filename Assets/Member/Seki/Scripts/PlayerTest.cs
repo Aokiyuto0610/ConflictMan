9a17bf0a256e86a03c3c3056573a584350bc083f
sekikaito <ttc22c1020033@edu.tech.ac.jp>
2024-10-25 18:38:45 +0900

    敵ダメージ受付Fix

--- Assets/Member/Seki/Scripts/PlayerTest.cs
+++ Assets/Member/Seki/Scripts/PlayerTest.cs
@@ -5,7 +5,31 @@ using UnityEngine;
 
 public class PlayerTest : MonoBehaviour
 {
-    [SerializeField, Label("弱点範囲に入っているか")] bool _weakness=false;
+    [SerializeField, Label("弱点範囲に入っているか")] public bool _weakness=false;
 
-    [SerializeField, Label("盾範囲に入ったのか")] bool _shield = false;
+    [SerializeField, Label("盾範囲に入ったのか")] public bool _shield = false;
+
+    //攻撃開始時にfalseに初期化して！！！
+    [SerializeField, Label("ダメージを与えた後か")] public bool _afterDamage=false;
+
+    //反射回数、停止時に初期化！
+    public int _reflection = 0;
+
+    //デバック用
+    [SerializeField] public int _playerHp = 9999;
+
+    /// <summary>
+    /// プレイヤーのHPを削る
+    /// </summary>
+    /// <param name="Damage"></param>
+    public void ReceivedDamage(int Damage)
+    {
+        /*
+            プレイヤーのHPを削る処理
+         */
+
+        //デバッグ用
+        _playerHp-=Damage;
+    }
+
 }
--- Assets/Member/Seki/Scripts/TagSetting.cs
+++ Assets/Member/Seki/Scripts/TagSetting.cs
@@ -7,15 +7,8 @@ public class TagSetting : MonoBehaviour
 {
     [SerializeField, Tag,Label("ê›íËÇ∑ÇÈÉ^ÉO")] string ThisObjTag;
 
-    // Start is called before the first frame update
     void Start()
     {
         this.gameObject.tag = ThisObjTag;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
