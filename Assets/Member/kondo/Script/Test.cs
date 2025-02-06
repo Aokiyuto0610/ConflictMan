9bd49ff04e4dbbf1afa010ce25bee021ac84f2c7
青木悠人 <TTC22C1020001@edu.tech.ac.jp>
2025-02-06 15:01:55 +0900

    修正

--- Assets/Member/kondo/Script/Test.cs
+++ Assets/Member/kondo/Script/Test.cs
@@ -1,29 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.Rendering;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System;
 
 
 public class Test : MonoBehaviour
 {
+    public FadeManager manager;
     public string SceneName;
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Button>().onClick.AddListener(() =>
-        {
-            SceneManager.LoadScene(SceneName);
-            SoundManager.Instance.PlaySE(SESoundData.SE.Button);
-
-        });
-
+        //SoundManager.Instance.PlaySE(SESoundData.SE.Button);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void Start_button()
+    {
+        //SceneChangr.scenechangrInstance._fade.SceneFade("Stage1");
+        SoundManager.Instance.PlaySE(SESoundData.SE.Button);
     }
 }
